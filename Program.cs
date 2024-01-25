be3fc216325db4bd8c5ee2ce50b89c76abe85627
Emmi Ari Ünver <emmiariunver@gmail.com>
2024-01-25 18:03:26 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace AudioOutputToSpeakers
+{
+    internal class Program
+    {
+        private static Task Main() => new Program().MainAsync();
+
+        [SuppressMessage("Performance", "CA1822:Mark members as static")]
+        private async Task MainAsync()
+        {
+            CoreAudioController audioController = new();
+
+            IEnumerable<CoreAudioDevice> outputDevices = await audioController.GetPlaybackDevicesAsync();
+            outputDevices = outputDevices.Where(outDev => outDev.State == DeviceState.Active);
+
+            CoreAudioDevice currentDefaultOutput = await audioController.GetDefaultDeviceAsync(DeviceType.Playback, Role.Multimedia);
+
+            if (currentDefaultOutput.FullName != "Speakers (USB PnP Audio Device)")
+                audioController.SetDefaultDevice(outputDevices.Where(outDev => outDev.FullName == "Speakers (USB PnP Audio Device)").First());
+        }
+    }
+}
